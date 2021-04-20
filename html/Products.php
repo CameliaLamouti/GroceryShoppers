e909320390ba6b8401fd3a53479ffec2d3fb2566
Younes <ygharbili@gmail.com>
2021-04-20 11:47:26 -0400

    Update Products.php

--- html/Products.php
+++ html/Products.php
@@ -53,24 +53,24 @@
                     <div class="row">
                         <div class="col-md-4">
                             <h3>Apples</h3>
-                            <a href="Products/Fruits and Vegetables/PP_Apples.html"> <img src =../Images/apples.jpg> </a>
+                            <a href="Products/Fruits and Vegetables/PP_Apples.php"> <img src =../Images/apples.jpg> </a>
                             $0.79/lb 
                         </br>
-                            <a href="Products/Fruits and Vegetables/PP_Apples.html"> More Description on Apples</a>
+                            <a href="Products/Fruits and Vegetables/PP_Apples.php"> More Description on Apples</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Bananas</h3>
-                            <a href="Products/Fruits and Vegetables/PP_Bananas.html"><img src="../Images/bananas.jpg"></a>
+                            <a href="Products/Fruits and Vegetables/PP_Bananas.php"><img src="../Images/bananas.jpg"></a>
                             Bananas are on sale! Only $0.79/lb
                         </br>
-                            <a href="Products/Fruits and Vegetables/PP_Bananas.html"> More Description on Bananas</a>
+                            <a href="Products/Fruits and Vegetables/PP_Bananas.php"> More Description on Bananas</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Broccoli</h3>
-                            <a href="Products/Fruits and Vegetables/PP_Broccoli.html"><img src="../Images/broccoli.jpg"></a>
+                            <a href="Products/Fruits and Vegetables/PP_Broccoli.php"><img src="../Images/broccoli.jpg"></a>
                             $1.58/lb
                         </br>
-                        <a href="Products/Fruits and Vegetables/PP_Broccoli.html"> More Description on Broccoli</a>
+                        <a href="Products/Fruits and Vegetables/PP_Broccoli.php"> More Description on Broccoli</a>
                         </div>
                     </div>
                 </div>
@@ -84,24 +84,24 @@
                     <div class="row">
                         <div class="col-md-4">
                             <h3>Salmon</h3>
-                            <a href="Products/Fish and Seafood/PP_Salmon.html"> <img src =../Images/salmon.jpg> </a>
+                            <a href="Products/Fish and Seafood/PP_Salmon.php"> <img src =../Images/salmon.jpg> </a>
                             $15.00/lb 
                         </br>
-                            <a href="Products/Fish and Seafood/PP_Salmon.html"> More Description on Salmon</a>
+                            <a href="Products/Fish and Seafood/PP_Salmon.php"> More Description on Salmon</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Tuna</h3>
-                            <a href="Products/Fish and Seafood/PP_Tuna.html"><img src="../Images/tuna.jpg"></a>
+                            <a href="Products/Fish and Seafood/PP_Tuna.php"><img src="../Images/tuna.jpg"></a>
                             $3.80/lb
                         </br>
-                            <a href="Products/Fish and Seafood/PP_Tuna.html"> More Description on Tuna</a>
+                            <a href="Products/Fish and Seafood/PP_Tuna.php"> More Description on Tuna</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Shrimp</h3>
-                            <a href="Products/Fish and Seafood/PP_Shrimp.html"><img src="../Images/shrimp.jpg"></a>
+                            <a href="Products/Fish and Seafood/PP_Shrimp.php"><img src="../Images/shrimp.jpg"></a>
                             $13.99/lb
                         </br>
-                        <a href="Products/Fish and Seafood/PP_Shrimp.html"> More Description on Shrimp</a>
+                        <a href="Products/Fish and Seafood/PP_Shrimp.php"> More Description on Shrimp</a>
                         </div>
                     </div>
                 </div>
@@ -115,24 +115,24 @@
                     <div class="row">
                         <div class="col-md-4">
                             <h3>Beef</h3>
-                            <a href="Products/Meat and Poultry/PP_Beef.html"> <img src =../Images/beef.jpg> </a>
+                            <a href="Products/Meat and Poultry/PP_Beef.php"> <img src =../Images/beef.jpg> </a>
                             Beef is on sale! Only $10.00/lb
                         </br>
-                            <a href="Products/Meat and Poultry/PP_Beef.html"> More Description on Beef</a>
+                            <a href="Products/Meat and Poultry/PP_Beef.php"> More Description on Beef</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Chicken</h3>
-                            <a href="Products/Meat and Poultry/PP_Chicken.html"><img src="../Images/chicken.jpg"></a>
+                            <a href="Products/Meat and Poultry/PP_Chicken.php"><img src="../Images/chicken.jpg"></a>
                             $3.50/lb
                         </br>
-                            <a href="Products/Meat and Poultry/PP_Chicken.html"> More Description on Chicken</a>
+                            <a href="Products/Meat and Poultry/PP_Chicken.php"> More Description on Chicken</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Pork</h3>
-                            <a href="Products/Meat and Poultry/PP_Pork.html"><img src="../Images/pork.jpg"></a>
+                            <a href="Products/Meat and Poultry/PP_Pork.php"><img src="../Images/pork.jpg"></a>
                             $5.56/lb
                         </br>
-                        <a href="Products/Meat and Poultry/PP_Pork.html"> More Description on Pork</a>
+                        <a href="Products/Meat and Poultry/PP_Pork.php"> More Description on Pork</a>
                         </div>
                     </div>
                 </div>
@@ -146,24 +146,24 @@
                     <div class="row">
                         <div class="col-md-4">
                             <h3>Milk</h3>
-                            <a href="Products/Dairy and Eggs/PP_Milk.html"> <img src =../Images/milk.jpg> </a>
+                            <a href="Products/Dairy and Eggs/PP_Milk.php"> <img src =../Images/milk.jpg> </a>
                             $3.61/liter
                         </br>
-                            <a href="Products/Dairy and Eggs/PP_Milk.html"> More Description on Milk</a>
+                            <a href="Products/Dairy and Eggs/PP_Milk.php"> More Description on Milk</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Cheese</h3>
-                            <a href="Products/Dairy and Eggs/PP_Cheese.html"><img src="../Images/cheese.jpg"></a>
+                            <a href="Products/Dairy and Eggs/PP_Cheese.php"><img src="../Images/cheese.jpg"></a>
                             $6.00/lb
                         </br>
-                            <a href="Products/Dairy and Eggs/PP_Cheese.html"> More Description on Cheese</a>
+                            <a href="Products/Dairy and Eggs/PP_Cheese.php"> More Description on Cheese</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Yogurt</h3>
-                            <a href="Products/Dairy and Eggs/PP_Yogurt.html"><img src="../Images/yogurt.jpg"></a>
+                            <a href="Products/Dairy and Eggs/PP_Yogurt.php"><img src="../Images/yogurt.jpg"></a>
                             $4.00/pack
                         </br>
-                        <a href="Products/Dairy and Eggs/PP_Yogurt.html"> More Description on Yogurt</a>
+                        <a href="Products/Dairy and Eggs/PP_Yogurt.php"> More Description on Yogurt</a>
                         </div>
                     </div>
                 </div>
@@ -177,24 +177,24 @@
                     <div class="row">
                         <div class="col-md-4">
                             <h3>Coke</h3>
-                            <a href="Products/Beverages/PP_Coke.html"> <img src =../Images/coke.jpg> </a>
+                            <a href="Products/Beverages/PP_Coke.php"> <img src =../Images/coke.jpg> </a>
                             $4.00/pack
                         </br>
-                            <a href="Products/Beverages/PP_Coke.html"> More Description on Coke</a>
+                            <a href="Products/Beverages/PP_Coke.php"> More Description on Coke</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Nestea</h3>
-                            <a href="Products/Beverages/PP_Nestea.html"><img src="../Images/nestea.jpg"></a>
+                            <a href="Products/Beverages/PP_Nestea.php"><img src="../Images/nestea.jpg"></a>
                             $2.99/pack
                         </br>
-                            <a href="Products/Beverages/PP_Nestea.html"> More Description on Nestea</a>
+                            <a href="Products/Beverages/PP_Nestea.php"> More Description on Nestea</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Crush</h3>
-                            <a href="Products/Beverages/PP_Crush.html"><img src="../Images/crush.jpeg"></a>
+                            <a href="Products/Beverages/PP_Crush.php"><img src="../Images/crush.jpeg"></a>
                             $1.49/can
                         </br>
-                        <a href="Products/Beverages/PP_Crush.html"> More Description on Crush</a>
+                        <a href="Products/Beverages/PP_Crush.php"> More Description on Crush</a>
                         </div>
                     </div>
                 </div>
@@ -208,24 +208,24 @@
                     <div class="row">
                         <div class="col-md-4">
                             <h3>Bread</h3>
-                            <a href="Products/Bread and Bakery/PP_Bread.html"> <img src =../Images/Bread.jpg> </a>
+                            <a href="Products/Bread and Bakery/PP_Bread.php"> <img src =../Images/Bread.jpg> </a>
                             $2.00/pack
                         </br>
-                            <a href="Products/Bread and Bakery/PP_Bread.html"> More Description on Bread</a>
+                            <a href="Products/Bread and Bakery/PP_Bread.php"> More Description on Bread</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Burger Buns</h3>
-                            <a href="Products/Bread and Bakery/PP_BurgerBuns.html"><img src="../Images/BurgerBuns.jpg"></a>
+                            <a href="Products/Bread and Bakery/PP_BurgerBuns.php"><img src="../Images/BurgerBuns.jpg"></a>
                             $4.99/pack
                         </br>
-                            <a href="Products/Bread and Bakery/PP_BurgerBuns.html"> More Description on Burger Buns</a>
+                            <a href="Products/Bread and Bakery/PP_BurgerBuns.php"> More Description on Burger Buns</a>
                         </div>
                         <div class="col-md-4">
                             <h3>Baguettes</h3>
-                            <a href="Products/Bread and Bakery/PP_Baguettes.html"><img src="../Images/baguette.jpeg"></a>
+                            <a href="Products/Bread and Bakery/PP_Baguettes.php"><img src="../Images/baguette.jpeg"></a>
                             Baguettes are on sale! Only $3.00/pack
                         </br>
-                        <a href="Products/Bread and Bakery/PP_Baguettes.html"> More Description on Baguettes</a>
+                        <a href="Products/Bread and Bakery/PP_Baguettes.php"> More Description on Baguettes</a>
                         </div>
                     </div>
                 </div>
